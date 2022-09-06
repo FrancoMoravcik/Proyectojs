00730bcd0edbde1e1666a5ea5825c2c7753059b9
unknown <francomoravcik12.fm@gmail.com>
2022-09-06 20:26:13 -0300

    trabajo simulador

--- miarchivo.js
+++ miarchivo.js
@@ -1,4 +1,4 @@
-
+/*
 let nombreIngresado = prompt("Ingrese su nombre");
 
 
@@ -25,4 +25,71 @@ for (let i = 1; i <= 2; i++) {
     (prompt("Porfavor Ingrese la marca de notebook que desea comprar(Asus, Apple, Samsung, Dell, Hp o Lenovo) "));
     prompt("Porfavor Indicanos de que modo desea abonarla(Tarjeta de Credito o Debito, Efectivo o transferencia bancaria)"); 
 }
+    }
+    */
+
+    let nombre = prompt("Ingrese su nombre")
+while(nombre == ""){
+    nombre = prompt("Porfavor ingrese su nombre")
+}while(nombre !== ""){
+    alert("hola " + nombre + " Bienvenido/a a nuestra pagina!")
+    break
+}
+
+let suma = (a,b) => {return a + b}
+
+let precioApple = 299999
+let precioDell = 347699
+let precioHp = 151981
+let precioAsus = 232888 
+let envio = 720
+
+let compra = "quiero comprar"
+let noCompra = "no gracias"
+
+    let marca = prompt("Que marca te gustaria comprar? las opciones que tenemos a dia de hoy son: Apple, Dell, Hp y Asus")
+switch (marca) {
+    case "apple" : 
+    alert("La notebook de marca Apple tiene un precio de $" + precioApple + " Con el envio quedaria en $" + suma(precioApple, envio)),
+    confirmacionDeCompra = prompt("Para confirmar compra escribe (quiero comprar), caso contrario escribe (no gracias)");
+    if (confirmacionDeCompra == compra) {
+        alert("Felicidades " + nombre +  " compraste un notebook APPLE")
+    }else if(confirmacionDeCompra == noCompra) {
+        alert("Lo esperamos la proxima, hasta luego " + nombre + "!");
+    }
+    break;
+
+    case "dell" :
+        alert("La notebook de marca Dell tiene un precio de $" + precioDell + " Con el envio quedaria en $" + suma(precioDell, envio)),
+        confirmacionDeCompra = prompt("Para confirmar compra escribe (quiero comprar), caso contrario escribe (no gracias)");
+        if (confirmacionDeCompra == compra) {
+            alert("Felicidades " + nombre +  " compraste un notebook DELL")
+        }else if(confirmacionDeCompra == noCompra) {
+            alert("Lo esperamos la proxima, hasta luego " + nombre + "!")
+        }
+        break;
+
+        case "hp" :
+        alert("La notebook de marca Hp tiene un precio de $" + precioHp + " Con el envio quedaria en $" + suma(precioHp, envio)),
+        confirmacionDeCompra = prompt("Para confirmar compra escribe (quiero comprar), caso contrario escribe (no gracias)");
+        if (confirmacionDeCompra == compra) {
+            alert("Felicidades " + nombre +  " compraste un notebook HP")
+        }else if(confirmacionDeCompra == noCompra) {
+            alert("Lo esperamos la proxima, hasta luego " + nombre + "!")
+        }
+        break;
+
+        case "asus" :
+            alert("La notebook de marca Asus tiene un precio de $" + precioAsus + " Con el envio quedaria en $" + suma(precioAsus, envio)),
+            confirmacionDeCompra = prompt("Si esta seguro de que quieres comprar escribe (quiero comprar), caso contrario escribe (no gracias)");
+            if (confirmacionDeCompra == compra) {
+                alert("Felicidades " + nombre +  " compraste un notebook ASUS")
+            }else if(confirmacionDeCompra == noCompra) {
+                alert("Lo esperamos la proxima, hasta luego " + nombre + "!")
+            }
+            break;
+
+            default : {
+                marca = prompt("Que marca te gustaria comprar? las opciones que tenemos a dia de hoy son: Apple, Dell, Hp y Asus")
+            } break;
     }
